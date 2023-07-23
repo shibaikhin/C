c11d417a47e95650d9597e23aa2b15cfad938e85
inextme-oldi <d.shibaikhin@ya.ru>
2023-07-24 01:31:31 +0300

    Добавлен вывод символа и строки

--- main.c
+++ main.c
@@ -5,8 +5,17 @@ void sayHallo()
     printf("Hello world!\n");
 }
 
+int myInt = 12;
+
+char ch = 'A';
+char str[] = "String";
+
 int main()
 {
     sayHallo();
+
+    printf("Символ %c\n", ch);
+    printf("Строка %s\n", str);
+
     return 0;
 }
